2a12bc3694cf75586db63916bec5991f95c6b2b7
Gefferson Casasola <gcasasolah@uni.pe>
2023-05-23 00:16:17 -0500

    Update Product Details

--- components/ProductDetailsCarousel.jsx
+++ components/ProductDetailsCarousel.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
+import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import Image from 'next/image';
-import bannerImg1 from '../public/banner-1.png'
+import Image from "next/image";
+import bannerImg1 from "../public/banner-1.png";
 
 const ProductDetailsCarousel = () => {
   return (
-    <div className='text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]'>
-        <Carousel
-        
-        infiniteLoop={true} 
-        showIndicators={false} 
-        showStatus={false}
-        thumbWidth={60}
-        className='productCarousel'
-        >
-            <Image className="" src={bannerImg1} alt="Course image detail" width="410" height="231"/>
-        </Carousel>
+    <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
+      <Image
+        className=""
+        src={bannerImg1}
+        alt="Course image detail"
+        width="820"
+        height="462"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetailsCarousel
+export default ProductDetailsCarousel;
